0464b3dc5fdd1fd20f12dee1466e611471f7004e
Jakson Alves de Aquino <jalvesaq@gmail.com>
2022-04-15 10:40:27 -0300

    Don't ignore existing bib files

--- python3/zotref.py
+++ python3/zotref.py
@@ -8,6 +8,7 @@ import sys
 import os
 import subprocess
 import io
+import tempfile
 import re
 import json
 from zotero import ZoteroEntries
@@ -29,13 +30,37 @@ if __name__ == "__main__":
     c = sorted(set(cites))
 
     if 'bibliography' in j['meta']:
-        # Rename the bib file to avoid overwriting the original one:
-        fnm = j['meta']['bibliography']['c'][0]['c']
-        if not fnm.find('zotcite.bib') == (len(fnm) - 11):
-            j['meta']['bibliography']['c'][0]['c'] = '/tmp/zotcite_' + fnm
+        b = j['meta']['bibliography']['c']
+        zbib = None
+        if isinstance(b, list):
+            for n in b:
+                nstr = ''
+                if isinstance(n['c'], list):
+                    nstr = n['c'][0]['c']
+                else:
+                    if isinstance(n['c'], str):
+                        nstr = n['c']
+                if nstr.find('zotcite.bib') == (len(nstr) - 11):
+                    zbib = nstr
+            if zbib is None:
+                zbib = tempfile.gettempdir() + '/' + os.getlogin() + '_zotcite.bib'
+                zitem = {'t': 'MetaInlines', 'c': [{'t': 'Str', 'c': zbib}]}
+                if j['meta']['bibliography']['t'] == 'MetaList':
+                    j['meta']['bibliography']['c'].append(zitem)
+                else:
+                    if j['meta']['bibliography']['t'] == 'MetaInlines':
+                        j['meta']['bibliography'] = {'t': 'MetaList', 'c': [j['meta']['bibliography'], zitem]}
+
         r = z.GetBib(c)
+
         # Save the bib file
-        with open(fnm, 'w') as f:
+        if os.getenv('QUARTO_FILTER_PARAMS'):
+            if os.path.exists(zbib):
+                sys.stderr.write('zotref (zotcite): overwriting "' + str(zbib) + '"\n')
+            else:
+                sys.stderr.write('zotref (zotcite): writing "' + str(zbib) + '"\n')
+            sys.stderr.flush()
+        with open(zbib, 'w') as f:
             f.write(r)
     else:
         # Get the references for the found citekeys formatted as the YAML and put
