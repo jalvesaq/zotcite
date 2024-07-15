e43143a022900edab69e1674099ecb2a7a52c1e1
Jakson Alves de Aquino <jalvesaq@gmail.com>
2024-07-15 00:14:39 -0300

    Hide Zotero key with legacy Vim syntax

--- lua/zotcite/config.lua
+++ lua/zotcite/config.lua
@@ -142,6 +142,7 @@ M.init = function()
 
     -- FIXME: Use treesitter
     vim.cmd([[
+    syn match zoteroCiteKey /@[A-Z0-9]\{8}#[:_[:digit:][:lower:][:upper:]\u00FF-\uFFFF]\+/ contains=zoteroHidden,zoteroVisible,@NoSpell containedin=pandocPCite
     syn match zoteroCiteKey /@[A-Z0-9]\{8}#[:_[:digit:][:lower:][:upper:]\u00FF-\uFFFF]\+/ contains=zoteroHidden,zoteroVisible,@NoSpell
     syn match zoteroHidden  /@[A-Z0-9]\{8}#/ contained containedin=zoteroCiteKey conceal
     syn match zoteroVisible /@[A-Z0-9]\{8}#\zs[:_[:digit:][:lower:][:upper:]\u00FF-\uFFFF]\+/ contained containedin=zoteroCiteKey
